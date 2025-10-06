46af0aa52cc9fccce155f0d385fe38c52ca72afd
Josuejg <cocho-cr@hotmail.com>
2025-10-05 22:40:04 -0600

    modificaciones en header , css, se crear  nueva vista CategoriasBarViewComponent y se unica con  CategoriasMenuViewComponent

--- ViewComponents/CategoriasBarViewComponentcs.cs
+++ ViewComponents/CategoriasBarViewComponentcs.cs
@@ -1,24 +1,23 @@
 ﻿using GraciaDivina.Models;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.Data.SqlClient;
-using System.Data;
 
 namespace GraciaDivina.ViewComponents
 {
-    public class CategoriasMenuViewComponent : ViewComponent
+    public class CategoriasBarViewComponent : ViewComponent
     {
         private readonly AccesoDatos _db;
-        public CategoriasMenuViewComponent(AccesoDatos db) => _db = db;
+        public CategoriasBarViewComponent(AccesoDatos db) => _db = db;
 
         public async Task<IViewComponentResult> InvokeAsync()
         {
-            var Lista = await _db.ConsultarAsync("gd_sp_Categoria_Menu",
+            var items = await _db.ConsultarAsync("gd_sp_Categoria_Menu",
                 dr => new Categoria
                 {
                     CategoriaID = dr.GetInt32(0),
                     Nombre = dr.GetString(1)
                 });
-            return View(Lista);
+
+            return View(items);
         }
     }
 }
